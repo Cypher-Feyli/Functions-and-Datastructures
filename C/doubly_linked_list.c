95336e7e9db846431812436f9bcb7207938d947e
Sayf.R <gusrashisa@student.gu.se>
2017-10-29 08:46:33 +0100

    Create doubly_linked_list.c

---
+++ C/doubly_linked_list.c
@@ -0,0 +1,130 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node  {
+	int data;
+	struct Node* next;
+	struct Node* prev;
+}nodex;
+
+
+nodex* createList(int x){
+    nodex * head = malloc(sizeof(nodex)); 
+    head->data = x;
+    head->next = NULL;
+    head->prev = NULL;
+    return head;
+}
+
+void addToFirst(nodex **list,int x){
+    
+    nodex * head = malloc(sizeof(nodex)); 
+    head->data = x;
+    head->next = *list;
+    head->prev = NULL;
+    (*list)->prev = head;
+    *list = head;
+}
+
+void addToLast(nodex **list, int val) {
+    nodex *newNode = malloc(sizeof(nodex));
+    nodex *current = *list;
+    newNode->data = val;
+    newNode->next = NULL;
+    if(*list ==  NULL){
+        newNode->prev = NULL;
+        *list = newNode;
+    }else{
+        while(current->next != NULL){
+            current= current->next;
+        }
+        current->next = newNode;
+        newNode->prev = current;
+    }
+}
+
+void removeByIndex(nodex **list,int index){
+    nodex * current = *list;
+    //temp_node the Node that we're going to free after we have removed it
+    nodex * temp_node = NULL;
+     if (index == 0) {
+         pop(list);
+    }else{
+         for(int i =0;i<index-1;i++){
+             if(current->next == NULL){
+                 break;
+             }else{
+                  current = current->next;
+             }
+         }
+         temp_node = current->next;
+         
+     //We re-link the nodes
+         current->next = temp_node->next;
+         temp_node->next->prev = temp_node->prev;
+         printf("%p",temp_node);
+         free(temp_node);
+    }
+}
+
+
+void printList(nodex * head) {
+    nodex * current = head;
+    printf("\n");
+
+    while (current != NULL) {
+        
+        printf("%d ", current->data);
+        current = current->next;
+        
+    }
+    printf("\n");
+     
+}
+void freeAll(nodex *list){
+     nodex * current = list;
+     while (current != NULL) {
+         printf("test  ");
+        current = current->next;
+        if(current != NULL){
+         free(current->prev);
+        }
+    }
+     printf("s\n");
+    free(current);
+}
+void pop(nodex ** head) {
+    nodex * next_node = NULL;
+
+    if (*head == NULL) {
+        printf("The list is empty");
+    }else{
+        next_node = (*head)->next;
+        free(*head);
+        *head = next_node;
+    }    
+}
+
+
+
+
+int main() {
+    //We create the linked list with the variable linked_list
+    nodex * linked_list=createList(7);
+    addToFirst(&linked_list,9);
+    addToFirst(&linked_list,3);
+   
+    addToLast(&linked_list,6);
+    printList(linked_list);
+    removeByIndex(&linked_list,1);
+    printf("\n");
+    printList(linked_list);
+    freeAll(linked_list);
+    printf("\n");
+    printList(linked_list);
+    pop(&linked_list);
+    printList(linked_list);
+    pop(&linked_list);
+    pop(&linked_list);
+    pop(&linked_list);
+}
